2785e7064867a0d4ce3427077a1e492e3ae10cff
Raja Kolli <rajadilipkolli@gmail.com>
2024-11-18 19:17:21 +0530

    Fix: issue with script

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 export OTEL_RESOURCE_ATTRIBUTES="service.name=coupon,service.instance.id=localhost:8080"
 export OTEL_LOGS_EXPORTER="otlp"
 
-java -Dotel.metric.export.interval=500 -Dotel.bsp.schedule.delay=500 -javaagent:"./build/agent/opentelemetry-javaagent.jar" -jar ./build/libs/coupon-0.0.1-SNAPSHOT.jar
+java -Dotel.metric.export.interval=500 -Dotel.bsp.schedule.delay=500 -javaagent:"./build/agent/opentelemetry-javaagent.jar" -jar ./build/libs/app.jar
